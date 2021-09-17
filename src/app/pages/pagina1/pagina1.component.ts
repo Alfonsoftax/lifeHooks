de27115e61e8558ef83cb1f07afc63f001aabb56
Alfonsoftax <alfonsoftax@gmail.com>
2021-09-17 19:03:55 +0200

    fin seccion ciclo de vidas

---
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,52 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styles: [
+  ]
+})
+export class Pagina1Component implements OnInit, OnChanges, DoCheck,AfterContentInit, AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy{
+  
+  
+  
+  nombre:string='Alfonso';
+  segundos:number=0;
+  timerSubscription!: Subscription;
+  constructor() {
+    console.log('Constructor');
+   }
+ 
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');  }
+  ngOnDestroy(): void {
+      console.log('ngOnDestroy'); 
+      this.timerSubscription.unsubscribe();
+      console.log('timerLimpiado'); 
+    
+    }
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.timerSubscription=interval(1000).subscribe(i=>{
+      this.segundos=i;  
+    })
+      
+  }
+
+  ngOnChanges(){
+    console.log('ngOnChanges');
+  }
+
+  guardar(){
+
+  }
+}
